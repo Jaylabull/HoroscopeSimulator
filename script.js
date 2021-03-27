b7b53aae68c8294b052ee3783a8d7b98b5ecb191
Jayla Bullard <jaylabullard@Jaylas-Air.attlocal.net>
2021-03-27 19:53:47 -0400

    updating CSS and adding JS API

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-const CHART_API = "https://json.astrologyapi.com/v1/western_horoscope";
+var API_key = "84145c01d8msh2e067ae17a0c02dp164871jsne31ed9b15a09";
+var API_URL = "https://vedicrishi-horoscope-and-astrology-v1.p.rapidapi.com/horo_chart/D1";
+
+var button = document.getElementById('submit-btn');
+
+button.addEventListener('click', function () {
+
+    searchForm.addEventListener('submit', submitSearch);
+
+    event.preventDefault();
+})
+
