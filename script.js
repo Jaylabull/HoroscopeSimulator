4bf3441567e3edf081f9e504bf69534d4b44c87e
myrriame <myrriametiffany@gmail.com>
2021-03-28 21:56:33 -0400

    added inner html

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-var API_KEY = "84145c01d8msh2e067ae17a0c02dp164871jsne31ed9b15a09";
-var API_URL = "https://vedicrishi-horoscope-and-astrology-v1.p.rapidapi.com/horo_chart/D1";
+// var API_KEY = "84145c01d8msh2e067ae17a0c02dp164871jsne31ed9b15a09";
+// var API_URL = "https://vedicrishi-horoscope-and-astrology-v1.p.rapidapi.com/horo_chart/D1";
 
 var button = document.getElementById('submit-btn');
 
@@ -8,8 +8,12 @@ button.addEventListener('click', function (event) {
 
     event.preventDefault();
 
-    function addContent (data) {
+    function addContent(data) {
         console.log(data);
+        var horoscope = document.createElement('p')
+        horoscope.innerHTML = data.description
+        document.body.appendChild(horoscope)
+
     }
 
 
@@ -17,13 +21,13 @@ button.addEventListener('click', function (event) {
     fetch('https://aztro.sameerkumar.website/?sign=aries&day=today', {
         method: 'POST'
     })
-    
 
- .then(reply => reply.json())
 
-.then(data => addContent(data))
+        .then(reply => reply.json())
 
-.catch(err => alert('Missing data, enter all information.'))
+        .then(data => addContent(data))
+
+        .catch(err => alert('Missing data, enter all information.'))
 
 });
 
