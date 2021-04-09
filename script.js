94c422c01322a88a7b9930fab080eb395080594b
myrriame <myrriametiffany@gmail.com>
2021-04-09 19:11:32 -0400

    fixed the zodiac container

--- script.js
+++ script.js
@@ -2,66 +2,58 @@ var button = document.getElementById('submit-btn');
 var usersName = document.getElementById('name-input').value
 
 
-const container = document.getElementsByClassName('container');
 const form = document.getElementById('form');
 
 button.addEventListener('click', function (event) {
     var selectZodiac = document.querySelector('#zodiac');
     var selectedZodiacValue = selectZodiac.options[selectZodiac.selectedIndex].value;
-
+    const container = document.getElementById('horoscope-card');
 
     event.preventDefault();
+
+    while (container.firstChild) {
+
+        container.removeChild(container.firstChild)
+    }
 
 
     function addContent(data) {
 
-        // console.log(data);
+
+        console.log(data);
 
 
-
-        data.forEach(data => {
-         var {date_range, description, color, mood} = data;
+        container.style.visibility = 'visible';
+        var horoscopediv = document.createElement('div')
 
         // alert(selectedZodiacValue);
+        var image = document.createElement('img')
+        image.src = 'resources/Zodiac_practice_pic.jpeg'
+        horoscopediv.appendChild(image)
 
-        const horoscope = document.createElement('div');
-        horoscope.classList.add('horoscope-card');
-        horoscope.innerHTML = `<div class="horoscope-card">
-        <img src="resources/Zodiac_practice_pic.jpeg" alt="zodiac sign image">
-        <h2 class = "zodiac-sign">Zodiac sign</h2>
-        <h2 class="${date_range}"></h2>
-        <h4 class = "horoscope">Horoscope</h4>
-         ${description}
-        <h4 class = "color">Today's Color</h4> 
-        ${color}
-        <h4 class = "mood">Today's Mood</h4>
-        ${mood}
-        </div>`
+        var sign = document.createElement('h2')
+        sign.innerHTML = selectedZodiacValue
+        horoscopediv.appendChild(sign)
 
-        container.appendChild(horoscope);
-    })
+        var dateRange = document.createElement('h2')
+        dateRange.innerHTML = data.date_range
+        horoscopediv.appendChild(dateRange)
+
+        var horoscope = document.createElement('h4')
+        horoscope.innerHTML = data.description
+        horoscopediv.appendChild(horoscope)
+
+        var color = document.createElement('h4')
+        color.innerHTML = data.color
+        horoscopediv.appendChild(color)
+
+        var mood = document.createElement('h4')
+        mood.innerHTML = data.mood
+        horoscopediv.appendChild(mood)
+
+        container.appendChild(horoscopediv)
+
     }
-
-
-        // alert(selectedZodiacValue);
-
-        // var dateRange = document.createElement('p')
-        // dateRange.innerHTML = data.date_range
-        // document.body.appendChild(dateRange)
-
-        // var horoscope = document.createElement('p')
-        // horoscope.innerHTML = data.description
-        // document.body.appendChild(horoscope)
-
-        // var color = document.createElement('p')
-        // color.innerHTML = data.color
-        // document.body.appendChild(color)
-
-        // var mood = document.createElement('p')
-        // mood.innerHTML = data.mood
-        // document.body.appendChild(mood)  
-
-
 
 
 
