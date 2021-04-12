7c97c5c1e76a0769696339f517550b2fa3b7fb1c
myrriame <myrriametiffany@gmail.com>
2021-04-11 21:35:30 -0400

    updating my branch

--- README.md
+++ README.md
@@ -1,18 +1,25 @@
 # ZodiacSimulator
-Day 1: The basic form, where users will input their birth day information, has been updated. 
+Day 1: The basic form, where users will input their birth day information, has been updated.
 
-Day 2: Added CSS to first get a visual of the div elements(which will be removed later) and to stack the form where users will enter in data. 
+Day 2: Added CSS to first get a visual of the div elements(which will be removed later) and to stack the form where users will enter in data.
 
-Day 3: Updated the background for the webpage and adjusted the form padding. Trying to figure out how to add an image icon. Resources added (i.e github icon). I added it as an image but going to look into adding it as a clickable icon. 
+Day 3: Updated the background for the webpage and adjusted the form padding. Trying to figure out how to add an image icon. Resources added (i.e github icon). I added it as an image but going to look into adding it as a clickable icon.
 
 Day 4: Added more input options. Started attmepting to incorporate a Birth Chart API. Also tweaked some CSS properties.
 
-Day 5: Condensed CSS input properties. Still adjusting layout of form. 
+Day 5: Condensed CSS input properties. Still adjusting layout of form.
 
-Day 6: Removed time aspect in html/css. Added fetch of API in JavaScript. Myrriame added function and defeined horoscope variable. 
+Day 6: Removed time aspect in html/css. Added fetch of API in JavaScript. Myrriame added function and defeined horoscope variable.
 
-
-Day 7: Define other variables to be included on page. 
+Day 7: Define other variables to be included on page.
 
 Day 8: Updated the form and Javascript to pull from zodiac drop down to generate daily horoscopes based on zodiac sign. Also changed footer links for github.
 
+Day 9: Created a Horoscope car that will appear once user fill in name and selects zodiac sign. CSS updates.
+
+Day 10: Fixed bug fixes in JS to display API content in a styled
+
+element
+Day 11: Removed unnecessary div elements. Changed the visiablity of the form when clciked. Trying to replace form with generated div.
+
+Day 12: We successfully made an if statement that changes the image based on the users zodiac sign and some CSS changes.
--- script.js
+++ script.js
@@ -1,39 +1,148 @@
 var button = document.getElementById('submit-btn');
-var usersName = document.getElementById('name-input').value
+// var usersName = document.getElementById('name-input').value
+
+const form = document.getElementById('form');
 
 button.addEventListener('click', function (event) {
     var selectZodiac = document.querySelector('#zodiac');
     var selectedZodiacValue = selectZodiac.options[selectZodiac.selectedIndex].value;
-
+    const container = document.getElementById('horoscope-card');
+    var usersName = document.getElementById('name-input').value
 
     event.preventDefault();
+
+    while (container.firstChild) {
+
+        container.removeChild(container.firstChild)
+    }
 
 
     function addContent(data) {
 
-        console.log(data);
 
+        // console.log(data);
+
+        var horoscopediv = document.createElement('div')
+
+        var sign = document.createElement('h1')
+        sign.innerHTML = selectedZodiacValue
+        horoscopediv.appendChild(sign)
+
+        var signarg = selectedZodiacValue
 
         // alert(selectedZodiacValue);
+        var name = document.createElement('h3')
+        name.innerHTML = `${usersName}'s horoscope for today!`
+        horoscopediv.appendChild(name)
 
-        var dateRange = document.createElement('p')
+        var image = document.createElement('img')
+        image.src = 'resources/Zodiac_practice_pic.jpeg'
+        horoscopediv.appendChild(image)
+
+        if (signarg === 'Capricorn') {
+            image.src = 'resources/Capricorn.jpeg'
+        } else if (signarg === 'Aquarius') {
+            image.src = 'resources/Aquarius.jpeg'
+        } else if (signarg === 'Pisces') {
+            image.src = 'resources/Pisces.jpeg'
+        } else if (signarg === 'Aries') {
+            image.src = 'resources/Aries.jpeg'
+        } else if (signarg === 'Taurus') {
+            image.src = 'resources/Taurus.jpeg'
+        } else if (signarg === 'Gemini') {
+            image.src = 'resources/Gemini.jpeg'
+        } else if (signarg === 'Cancer') {
+            image.src = 'resources/Cancer.jpeg'
+        } else if (signarg === 'Leo') {
+            image.src = 'resources/Leo.jpeg'
+        } else if (signarg === 'Virgo') {
+            image.src = 'resources/Virgo.jpeg'
+        } else if (signarg === 'Libra') {
+            image.src = 'resources/Libra.jpeg'
+        } else if (signarg === 'Scorpio') {
+            image.src = 'resources/Scorpio.jpeg'
+        } else if (signarg === 'Sagittarius') {
+            image.src = 'resources/Sagittarius.jpeg'
+        } else {
+            image.src = 'resources/Zodiac_practice_pic.jpeg'
+        }
+
+        // var signPic = function (img) {
+
+        //     image.src = img;
+        //     horoscopediv.appendChild(pic);
+
+        //     var checkSign = function (signarg) {
+        //         switch (signarg) {
+        //             case 'Capricorn':
+        //                 signPic('resources/Capricorn.jpeg')
+        //                 break;
+        //             case 'Aquarius':
+        //                 signPic('resources/Aquarius.jpeg')
+        //                 break;
+        //             case 'Pisces':
+        //                 signPic('resources/Pisces.jpeg')
+        //                 break;
+        //             case 'Aries':
+        //                 signPic('resources/Aries.jpeg')
+        //                 break;
+        //             case 'Taurus':
+        //                 signPic('resources/Taurus.jpeg')
+        //                 break;
+        //             case 'Gemini':
+        //                 signPic('resources/Gemini.jpeg')
+        //                 break;
+        //             case 'Cancer':
+        //                 signPic('resources/Cancer.jpeg')
+        //                 break;
+        //             case 'Leo':
+        //                 signPic('resources/Leo.jpeg')
+        //                 break;
+        //             case 'Virgo':
+        //                 signPic('resources/Virgo.jpeg')
+        //                 break;
+        //             case 'Libra':
+        //                 signPic('resources/Libra.jpeg')
+        //                 break;
+        //             case 'Scorpio':
+        //                 signPic('resources/Scorpio.jpeg')
+        //                 break;
+        //             case 'Sagittarius':
+        //                 signPic('resources/Sagitarius.jpeg')
+        //                 break;
+        //             default: signPic('resources/Zodiac_practice_pic.jpeg')
+        //                 break;
+        //         }
+        //     };
+        //     checkSign(signarg);
+        // };
+
+
+        // var sign = document.createElement('h2')
+        // sign.innerHTML = selectedZodiacValue
+        // horoscopediv.appendChild(sign)
+
+        var dateRange = document.createElement('h2')
         dateRange.innerHTML = data.date_range
-        document.body.appendChild(dateRange)
+        horoscopediv.appendChild(dateRange)
 
-        var horoscope = document.createElement('p')
+        var horoscope = document.createElement('h4')
         horoscope.innerHTML = data.description
-        document.body.appendChild(horoscope)
+        horoscopediv.appendChild(horoscope)
 
-        var color = document.createElement('p')
-        color.innerHTML = data.color
-        document.body.appendChild(color)
+        var color = document.createElement('h4')
+        color.innerHTML = `Color: ${data.color}`
+        horoscopediv.appendChild(color)
 
-        var mood = document.createElement('p')
-        mood.innerHTML = data.mood
-        document.body.appendChild(mood)  
+        var mood = document.createElement('h4')
+        mood.innerHTML = `Mood: ${data.mood}`
+        horoscopediv.appendChild(mood)
+
+        // sign.style.backgroundColor = data.color;
+
+        container.appendChild(horoscopediv)
+
     }
-
-
 
 
     fetch(`https://aztro.sameerkumar.website/?sign=${selectedZodiacValue}&day=today`, {
@@ -47,6 +156,5 @@ button.addEventListener('click', function (event) {
         .catch(err => alert('Missing data, enter all information.'))
 
 });
-
 
 
